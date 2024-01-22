5d51fe3ed1989d94d0912723d7087ea9f4ca94aa
Gregory Marshall <gmarshall@dawan.fr>
2024-01-22 17:27:35 +0100

    added service

---
+++ src/app/service/api-service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+    private readonly API_URL = "http://localhost:8080";
+
+    
+}
